bd1499149426ac88c2bd59dbb93f3644431ff174
Jacob Egestad <jacob.egestad@live.com>
2022-02-03 22:18:14 -0700

    tabs superior

--- Prog2.java
+++ Prog2.java
@@ -19,59 +19,59 @@ import java.nio.ByteBuffer;
  */
 
 public class Prog2 {
-    // some constants and class values copied from Jacob Egestad's Program 1
-    private static final int STRING_FIELDS = 9; // number of string fields
+	// some constants and class values copied from Jacob Egestad's Program 1
+	private static final int STRING_FIELDS = 9; // number of string fields
 	private static final int INT_FIELDS = 4; // number of int fields
-    private static final int INT_FIELDS_LENGTHS = INT_FIELDS * Integer.BYTES; // length of int fields
-    private static final int ENTRIES_PER_BUCKET = 50; // number of entries per bucket
+	private static final int INT_FIELDS_LENGTHS = INT_FIELDS * Integer.BYTES; // length of int fields
+	private static final int ENTRIES_PER_BUCKET = 50; // number of entries per bucket
 	private static int[] stringFieldLengths = new int[STRING_FIELDS]; // lengths of the string fields
 	private static int projectSize = -1; // size of projects in bytes
-    private static int bucketSize = -1; // size of bucket in bytes
+	private static int bucketSize = -1; // size of bucket in bytes
 	private static int numProjects = -1; // number of projects
 
-    public static void main(String[] args){
-        if(args.length == 0) {
-            printErrAndExit("Prog2 <path/to/binary_file>");
-        }
-        File inputFile = new File(args[0]);
-        if(!inputFile.isFile()){
-            printErrAndExit("Prog2 <path/to/binary_file>");
-        }
-        try {
-            RandomAccessFile inputRAF = new RandomAccessFile(inputFile, "r");
-            RandomAccessFile hashBucketRAF = new RandomAccessFile("HashBucket.bin", "w");
-            inputRAF.close();
-            hashBucketRAF.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-            printErrAndExit("Can't access " + args[0]);
-        }
-    }
+	public static void main(String[] args){
+		if(args.length == 0) {
+			printErrAndExit("Prog2 <path/to/binary_file>");
+		}
+		File inputFile = new File(args[0]);
+		if(!inputFile.isFile()){
+			printErrAndExit("Prog2 <path/to/binary_file>");
+		}
+		try {
+			RandomAccessFile inputRAF = new RandomAccessFile(inputFile, "r");
+			RandomAccessFile hashBucketRAF = new RandomAccessFile("HashBucket.bin", "w");
+			inputRAF.close();
+			hashBucketRAF.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+			printErrAndExit("Can't access " + args[0]);
+		}
+	}
 
-    /**
-     * idToKey: Takes a String Project ID and converts it into an integer key. It does this by
-     * reversing the String and appending the least significant digit of the ASCII value of
-     * each character in the String.
-     * Pre-condition: The binary file is being read.
-     * Post-condition: The 
-     * @param id
-     * @return
-     */
-    public static String idToKey(String id){
-        String key = ""; // key to be converted to an integer for the key
-        for(int i = id.length() - 1; i >= 0; i--){
-            key += ((int) id.charAt(i)) % 10;
-        }
-        return key;
-    }
-    
-    /**
+	/**
+	 * idToKey: Takes a String Project ID and converts it into an integer key. It does this by
+	 * reversing the String and appending the least significant digit of the ASCII value of
+	 * each character in the String.
+	 * Pre-condition: The binary file is being read.
+	 * Post-condition: The 
+	 * @param id
+	 * @return
+	 */
+	public static String idToKey(String id){
+		String key = ""; // key to be converted to an integer for the key
+		for(int i = id.length() - 1; i >= 0; i--){
+			key += ((int) id.charAt(i)) % 10;
+		}
+		return key;
+	}
+	
+	/**
 	 * fillFieldLengths: fills stringFieldLengths array with lengths of each of the string fields of
 	 * the projects in inputRAF, as well as projectSize, bucketSize and numProjects
 	 * Pre-conditions: inputRAF is an existing, properly formatted binary file
 	 * Post-conditions: stringFieldLengths, projectSize, numProjects are filled with values
 	 * from end of file inputRAF
-     * Note: Copied from Jacob Egestad's Program 1
+	 * Note: Copied from Jacob Egestad's Program 1
 	 * @param inputRAF RandomAccessFile for dealing with binary file from user
 	 * @return void
 	 */
@@ -98,14 +98,14 @@ public class Prog2 {
 			//System.out.println("Field " + i + " length: " + stringFieldLengths[i]);
 		}
 		numProjects = (int) (startOfLengths / projectSize);
-        bucketSize = (stringFieldLengths[0] + Long.BYTES) * ENTRIES_PER_BUCKET;
+		bucketSize = (stringFieldLengths[0] + Long.BYTES) * ENTRIES_PER_BUCKET;
 	}
 
-    /**
+	/**
 	 * bytesToString: Convert an array of bytes to a string
 	 * Pre-condition: N/A
 	 * Post-condition: N/A
-     * Note: Copied from Jacob Egestad's Program 1
+	 * Note: Copied from Jacob Egestad's Program 1
 	 * @param bytes string field length to be converted to string
 	 * @return bytes but represented as a String
 	 */
@@ -121,7 +121,7 @@ public class Prog2 {
 	 * bytesToInt: converts an array of bytes to an int
 	 * Pre-condition: bytes is of length 4
 	 * Post-condition: N/A
-     * Note: Copied from Jacob Egestad's Program1
+	 * Note: Copied from Jacob Egestad's Program1
 	 * @param bytes array of bytes representing a tring
 	 * @return int value of the the bytes
 	 */
@@ -129,11 +129,11 @@ public class Prog2 {
 		return ByteBuffer.wrap(bytes).getInt();
 	}
 
-    /**
+	/**
 	 * printErrAndExit: prints provided string to stderr and exits with error code 1
 	 * Pre-conditions: The user has given bad input by not including a valid command line argument
 	 * Post-conditions: This method exits the program
-     * Note: copied from Jacob Egestad's Program 1
+	 * Note: copied from Jacob Egestad's Program 1
 	 * @param message string to print to stderr
 	 * @return void
 	 */
--- Prog2q.java
+++ Prog2q.java
@@ -1,3 +1,3 @@
 public class Prog2q {
-    
+	
 }
