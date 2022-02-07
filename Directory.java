eaf26519bc01b98a3954d6d97a7e077a3d2051dc
cademarks <DickensConklinMisurda2332>
2022-02-06 18:31:53 -0700

    Fleshed out and commented ExtendibleHashIndex, Directory, HashBucket, and HashEntry

--- Directory.java
+++ Directory.java
@@ -1,15 +1,110 @@
+/**
+ * @author Jacob Egestad & Cade Marks
+ * File: Directory.java
+ * Course name: CSC 460 - Database Design
+ * Assignment Title: Program #2: Extendible Hashing
+ * Instructor: Lester McCann
+ * TAs: Haris Riaz, Aayush Pinto
+ * Due Date: February 9, 2022
+ *
+ * Description: A directory of addresses to hash buckets in the hash bucket file. Stores the
+ *              prefixes and addresses of each bucket in the file. When buckets split, if there
+ *              isn't already a prefix for the new buckets in the directory, the directory grows
+ *              by a factor of 10 to fit new buckets/prefixes.
+ *
+ * Known deficiencies: N/A
+ *
+ * Requirements: Java 16
+ */
+
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Set;
+
 
 public class Directory {
+    // attributes
     HashMap<String, Long> directory;
+    int prefixSize;
 
+    // constructor
     public Directory() {
         directory = new HashMap<String, Long>();
-        // add initial 10 buckets to HashBucket file
+        prefixSize = 1;
     }
 
-    // use if directory must grow to include an additional digit for each prefix
-    public void grow() {
+    // getter method for prefixSize
+    public int getPrefixSize() {
+        return prefixSize;
+    }
 
+    /**
+     * getAddress(String idKey)
+     * Description: Returns the address of the bucket with the prefix matching the idKey
+     * Preconditions: Directory was initialized with 10 buckets
+     * Postconditions: N/A
+     * @param idKey String index key found from entry's projectID
+     * @return long representing address of correct hash bucket, -1 if not found
+     */
+    public long getAddress(String idKey) {
+        for(String key : directory.keySet()) {
+            if (idKey.startsWith(key)) {
+                return directory.get(key);
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * addAddress(String prefix, long addr)
+     * Description: Inserts a new prefix and address key/value pair
+     * Preconditions: Directory was initialized with 10 buckets
+     *                Given prefix is not already a key in the map
+     * Postconditions: N/A
+     * @param prefix String prefix key to add to map
+     * @param addr long address value to add to map
+     * @return void
+     */
+    public void addAddress(String prefix, long addr) {
+        directory.put(prefix, addr);
+    }
+
+    /**
+     * changeAddress(String prefix, long newAddr)
+     * Description: Changes the address value for a given prefix
+     * Preconditions: Directory was initialized with 10 buckets
+     *                Given prefix is already a key in the map
+     * Postconditions: N/A
+     * @param prefix String prefix key to modfiy in map
+     * @param newAddr long address value to mopdify to map
+     * @return void
+     */
+    public void changeAddress(String prefix, long newAddr) {
+        directory.replace(prefix, newAddr);
+    }
+
+    /**
+     * grow()
+     * Description: Grow directory by a factor of 10, making new prefixes by adding digits 0-9 to
+     *              the end of each old prefix. This is done to accomodate new hash buckets in the
+     *              case a bucket is split after reaching capacity.
+     * Preconditions: Directory was initialized with 10 buckets
+     * Postconditions: N/A
+     * @return void
+     */
+    public void grow() {
+        prefixSize++;
+        List<String> prefixes = new ArrayList<String>();
+        Set<String> prevKeys = directory.keySet();
+        // get all key values then loop 10 times on each adding 0-9 to the end of each for new keys
+        HashMap<String, Long> newDirectory = new HashMap<String, Long>();
+        for (String key : prevKeys) {
+            for (int i = 0; i < 10; i++) {
+                newDirectory.put(key+i, directory.get(key));
+            }
+        }
+        directory = newDirectory;
+        System.out.println(newDirectory.keySet());
     }
 }
--- HashBucket.java
+++ HashBucket.java
@@ -1,43 +1,72 @@
+/**
+ * @author Jacob Egestad & Cade Marks
+ * File: HashBucket.java
+ * Course name: CSC 460 - Database Design
+ * Assignment Title: Program #2: Extendible Hashing
+ * Instructor: Lester McCann
+ * TAs: Haris Riaz, Aayush Pinto
+ * Due Date: February 9, 2022
+ *
+ * Description: Represents a single bucket, storing all the record entries beginning with a
+ *              specific prefix. If the bucket reaches full capacity (50 entries) and another
+ *              needs to be inserted, the bucket will be split into 10 new buckets, each with the
+ *              same prefix plus an additional digit (0-9). These buckets are all stored in a hash
+ *              bucket file, and the addresses are saved in a Directory saved in the Extendible
+ *              Hash Index.
+ *
+ * Known deficiencies: N/A
+ *
+ * Requirements: Java 16
+ */
+
 public class HashBucket {
+    // attributes
     private final int CAPACITY = 50;
     private String prefix; // the initial digits of values stored in this bucket
     HashEntry[] entries;
     int numEntries;
 
+    // constructor
     public HashBucket(String prefix) {
         this.prefix = prefix;
         entries = new HashEntry[CAPACITY];
         numEntries = 0;
     }
 
+    // getter method for prefix
     public String getPrefix() {
         return prefix;
     }
 
+    // getter method for entries
     public HashEntry[] getEntries() {
         return entries;
     }
 
-    public void insert(String projID, long dbAddress) {
-        if (numEntries == CAPACITY) {
-            // check if directory needs to grow
-            // split the bucket into 10 new buckets
-        } else {
-            // add the new entry
-            HashEntry newEntry = new HashEntry(projID, dbAddress);
-            entries[numEntries] = newEntry;
-            numEntries++;
-        }
+
+    /**
+     * isFull()
+     * Description: returns true if the bucket is at full capacity, else false
+     * Preconditions: constant CAPACITY represents maximum amount of entries per bucket
+     *                numEntries is updated each time an entry is added
+     * Postconditions: N/A
+     * @return numEntries Boolean value representing whether bucket is full
+     */
+    public Boolean isFull() {
+        return numEntries == CAPACITY;
     }
 
-
-    private class HashEntry {
-        String projID;
-        long dbAddress;
-
-        private HashEntry(String projID, long dbAddress) {
-            this.projID = projID;
-            this.dbAddress = dbAddress;
-        }
+    /**
+     * insert(HashEntry newEntry)
+     * Description: adds a new entry to the bucket
+     * Preconditions: The bucket is not full
+     * Postconditions: N/A
+     * @param newEntry HashEntry to be added to bucket
+     * @return void
+     */
+    public void insert(HashEntry newEntry) {
+        // add the new entry
+        entries[numEntries] = newEntry;
+        numEntries++;
     }
 }
