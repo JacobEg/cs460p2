840ea660b53d2f7731e847b37b4ed7d62318de27
Jacob Egestad <jacob.egestad@live.com>
2022-02-09 18:02:57 -0700

    moved fillFieldLengths to EHI, print correctly

--- Prog2q.java
+++ Prog2q.java
@@ -19,15 +19,6 @@ import java.io.RandomAccessFile;
 import java.util.Scanner;
 
 public class Prog2q {
-    private static final int STRING_FIELDS = 9; // number of string fields
-	private static final int INT_FIELDS = 4; // number of int fields
-	private static final int INT_FIELDS_LENGTHS = INT_FIELDS * Integer.BYTES; // length of int fields
-	private static final int ENTRIES_PER_BUCKET = 50; // number of entries per bucket
-	private static int[] stringFieldLengths = new int[STRING_FIELDS]; // lengths of the string fields
-	private static int projectSize = -1; // size of projects in bytes
-	private static int bucketSize = -1; // size of bucket in bytes
-	private static int numProjects = -1; // number of projects
-	public static int entrySize = -1;
 
     public static void main(String[] args){
         if(args.length == 0){
@@ -44,7 +35,6 @@ public class Prog2q {
         }
         try{
             RandomAccessFile inputRAF = new RandomAccessFile(inputFile, "r");
-            fillFieldLengths(inputRAF);
             RandomAccessFile hashBucketRAF = new RandomAccessFile(hashBucketFile, "r");
             queryUser(inputRAF, hashBucketRAF);
             inputRAF.close();
@@ -66,7 +56,7 @@ public class Prog2q {
      * @return void
      */
     public static void queryUser(RandomAccessFile inputRAF, RandomAccessFile hashBucketRAF){
-        ExtendibleHashIndex extendibleHashIndex = new ExtendibleHashIndex(hashBucketRAF, inputRAF, bucketSize, "r");
+        ExtendibleHashIndex extendibleHashIndex = new ExtendibleHashIndex(hashBucketRAF, inputRAF, "r");
         Scanner userInput = new Scanner(System.in);
         System.out.println("Enter ProjectID suffix to search for (-1 to quit):");
         while(userInput.hasNextLine()){
@@ -79,41 +69,4 @@ public class Prog2q {
         }
         userInput.close();
     }
-
-    /**
-	 * fillFieldLengths: fills stringFieldLengths array with lengths of each of the string fields of
-	 * the projects in inputRAF, as well as projectSize, bucketSize, entrySize and numProjects
-	 * Pre-conditions: inputRAF is an existing, properly formatted binary file
-	 * Post-conditions: stringFieldLengths, projectSize, numProjects are filled with values
-	 * from end of file inputRAF
-	 * Note: Copied from Jacob Egestad's Program 1
-	 * @param inputRAF RandomAccessFile for dealing with binary file from user
-	 * @return void
-	 */
-	public static void fillFieldLengths(RandomAccessFile inputRAF){
-		byte[] lengths = new byte[STRING_FIELDS * Integer.BYTES]; // 9 lengths in a binary string
-		long startOfLengths = -1; // to be used to mark the beginning of the string field lengths
-		try {
-			startOfLengths = inputRAF.length() - (STRING_FIELDS * Integer.BYTES);
-			inputRAF.seek(startOfLengths);
-			inputRAF.read(lengths);
-			inputRAF.seek(0);
-		} catch (Exception e) {
-			Prog2.printErrAndExit("Error reading input file");
-		}
-		projectSize = INT_FIELDS_LENGTHS; // 4*4 to start with
-		for(int i = 0; i < STRING_FIELDS; i++){
-			int j = i  * Integer.BYTES; // the index of the byte to be gotten from lengths to fill toInt
-			byte[] toInt = new byte[Integer.BYTES]; // array representing int value of field i
-			for(int k = 0; k < Integer.BYTES; k++){
-				toInt[k] = lengths[j++];
-			}
-			stringFieldLengths[i] = Prog2.bytesToInt(toInt);
-			projectSize += stringFieldLengths[i];
-			//System.out.println("Field " + i + " length: " + stringFieldLengths[i]);
-		}
-		numProjects = (int) (startOfLengths / projectSize);
-		entrySize = stringFieldLengths[0] + Long.BYTES;
-		bucketSize = entrySize * ENTRIES_PER_BUCKET;
-	}
 }
